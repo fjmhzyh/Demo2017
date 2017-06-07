bfa70aef05947b8f8f219a7a99400801799ee9d8
fjm <995119598@qq.com>
2017-06-07 22:43:28 +0800

    111

--- 服务器.js
+++ 服务器.js
@@ -11,3 +11,17 @@ http://taobo.com/products/skirt-6202
 
 
 url 中的域名不区分大小写，但是路径和文件名可能会区分大小写
+
+
+
+
+// session 和 cookie
+设置httponly 以后 ，客户端JS无法读取到，也无法看到cookie
+
+服务端session丢失以后，客户端将退出登录
+同一用户的每次登录都会生成一个不一样的session
+
+session 中间件会在 req 上添加 session 对象，即 req.session 初始值为 {}。
+当我们登录后设置 req.session.user = 用户信息，返回浏览器的头信息中会带上 
+set-cookie 将 session id 写到浏览器 cookie 中，那么该用户下次请求时，通过
+带上来的 cookie 中的 session id 我们就可以查找到该用户，并将用户信息保存到 req.session.user。
